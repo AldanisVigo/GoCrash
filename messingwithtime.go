3a89e903b1225172ae48d16c3bac9f1cc1574aed
Aldanis Vigo <aldanisvigo@gmail.com>
2022-11-08 21:44:58 -0600

    quick refactor on the messingwithdates.go file

--- messingwithtime.go
+++ messingwithtime.go
@@ -12,21 +12,19 @@ import(
 func main(){
 	fmt.Println("Please enter a date (MM/DD/YYYY): ")
 	reader := bufio.NewReader(os.Stdin)
-	date,err := reader.ReadString('\n') 
-	if err == nil {
-		//Old Way
-		// fmt.Println("You typed in " + date)
-		// month,_ := strconv.Atoi(strings.Split(date,"/")[0])
-		// day,_ := strconv.Atoi(strings.Split(date,"/")[1])
-		// year,_ := strconv.Atoi(strings.Split(date,"/")[2])
-		
-		// newDate := time.Date(year,time.Month(month),day,0,0,0,0,time.UTC)
-		// fmt.Println(newDate.Weekday())
+	date,_ := reader.ReadString('\n') 
+	
+	//Old Way
+	// fmt.Println("You typed in " + date)
+	// month,_ := strconv.Atoi(strings.Split(date,"/")[0])
+	// day,_ := strconv.Atoi(strings.Split(date,"/")[1])
+	// year,_ := strconv.Atoi(strings.Split(date,"/")[2])
+	
+	// newDate := time.Date(year,time.Month(month),day,0,0,0,0,time.UTC)
+	// fmt.Println(newDate.Weekday())
 
-		//New Way
-		date, _ := time.Parse(date,"")
-		fmt.Println(date.Weekday())
-	}else{
-		fmt.Println("Error")
-	}
+	//New Way
+	myDay, _ := time.Parse(date,"")
+	fmt.Println(myDay.Weekday())
+
 }
